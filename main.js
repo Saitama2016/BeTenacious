f7e457127d6622e4d1ddfde53ff09160da26da5a
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-05-19 15:03:11 -0700

    Making Progress!!! :)
    Getting the basics of jQuery click functions! Let's keep going!!

--- main.js
+++ main.js
@@ -1,12 +1,67 @@
-$.getJSON( "https://wger.de/api/v2/exercise/?page=2", function( data ) {
-    console.log('It works!', data);
-    // var items = [];
-    // $.each( data, function( key, val ) {
-    //   items.push( "<li id='" + key + "'>" + val + "</li>" );
-    // });
-   
-    // $( "<ul/>", {
-    //   "class": "my-new-list",
-    //   html: items.join( "" )
-    // }).appendTo( "body" );
+$( () => {
+  console.log('loaded');
+
+
+  $.getJSON( "https://wger.de/api/v2/exercise/?page=2", function( data ) {
+      console.log('It works!', data);
+      // var items = [];
+      // $.each( data, function( key, val ) {
+      //   items.push( "<li id='" + key + "'>" + val + "</li>" );
+      // });
+
+      // $( "<ul/>", {
+      //   "class": "my-new-list",
+      //   html: items.join( "" )
+      // }).appendTo( "body" );
   });
+
+  $('#fitness').click(function(e) {
+    $('#fitnessPage').show();
+    $('#workoutPage').hide();
+    $('#nutritionPage').hide();
+    $('#restaurantPage').hide();
+    $('#meetUpPage').hide();
+  });
+  
+  $('#workout').click(function(e) {
+    $('#workoutPage').show();
+    $('#fitnessPage').hide();
+    $('#nutritionPage').hide();
+    $('#restaurantPage').hide();
+    $('#meetUpPage').hide();
+  });
+  
+  $('#nutrition').click(function(e) {
+    $('#nutritionPage').show();
+    $('#fitnessPage').hide();
+    $('#workoutPage').hide();
+    $('#restaurantPage').hide();
+    $('#meetUpPage').hide();
+  });
+  
+  $('#restaurant').click(function(e) {
+    $('#restaurantPage').show();
+    $('#fitnessPage').hide();
+    $('#workoutPage').hide();
+    $('#nutritionPage').hide();
+    $('#meetUpPage').hide();
+  });
+  
+  $('#meetups').click(function(e) {
+    $('#meetUpPage').show();
+    $('#fitnessPage').hide();
+    $('#workoutPage').hide();
+    $('#restaurantPage').hide();
+    $('#nutritionPage').hide();
+  });
+  
+  $('.fa-home').click(function(e) {
+    $('#meetUpPage').hide();
+    $('#fitnessPage').hide();
+    $('#workoutPage').hide();
+    $('#restaurantPage').hide();
+    $('#nutritionPage').hide();
+  });
+
+});
+
